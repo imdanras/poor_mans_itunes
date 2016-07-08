d50fc563293f99835ffe4d46fc01a88321120beb
Daniel Rasmussen <dan.r.rasmussen@gmail.com>
2016-07-08 16:36:32 -0700

    working itunes search api call

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,8 @@
+class MainController < ApplicationController
+def index
+ 	@data = RestClient.get('http://itunes.apple.com/search', {
+  	params: { term: params[:term] }
+  	})
+ 	@data = JSON.parse(@data)['results']
+  end
+end
